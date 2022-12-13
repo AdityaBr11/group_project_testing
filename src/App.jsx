5bde67e4e797cb7606c106c18e7ab9420f9e7753
apurvaBr11 <apurvkashyappurnea@gmail.com>
2022-12-13 13:42:05 +0530

    hhh

--- src/App.jsx
+++ src/App.jsx
@@ -8,59 +8,7 @@ function App() {
   console.log(appData);
   return (
     <div>
-      <Heading textAlign={"center"} color={"blue.400"} size={"2xl"}>
-        Frontend React Template
-      </Heading>
-      <Heading size={"lg"} textAlign={"center"} color={"red.400"} mt={5}>
-        Created with ❤ by Prajwal Jaiswal
-      </Heading>
-
-      <Box
-        width={"50%"}
-        m={"auto"}
-        mt={10}
-        display={"flex"}
-        flexDirection={"column"}
-        alignItems={"center"}
-        p={5}
-        borderRadius={10}
-        boxShadow={"xl"}
-      >
-        <Heading color={"blue.400"}>
-          <ArrowLeftIcon />
-          -- Follow me on --
-          <ArrowRightIcon />{" "}
-        </Heading>
-        <HStack mt={5}>
-          <Button
-            color={"red.400"}
-            size={"lg"}
-            bgColor={"transparent"}
-            _hover={{ bgColor: "transparent" }}
-            rightIcon={<TfiGithub />}
-          >
-            GitHub
-          </Button>
-          <Button
-            color={"red.400"}
-            size={"lg"}
-            bgColor={"transparent"}
-            _hover={{ bgColor: "transparent" }}
-            rightIcon={<TfiLink />}
-          >
-            Medium
-          </Button>
-          <Button
-            color={"red.400"}
-            size={"lg"}
-            bgColor={"transparent"}
-            _hover={{ bgColor: "transparent" }}
-            rightIcon={<TfiLinkedin />}
-          >
-            LinkedIn
-          </Button>
-        </HStack>
-      </Box>
+      aggdfgdf
     </div>
   );
 }
